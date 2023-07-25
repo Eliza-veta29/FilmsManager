8809fb4ef3205824cf75c5a413a12625cb730fde
Liza <skomorohova-elizaveta@rambler.ru>
2023-07-25 11:15:34 +0300

    films

---
+++ src/main/java/Poster.java
@@ -0,0 +1,14 @@
+public class Poster {
+    private int id;
+    private String name;
+    private String picture;
+    private boolean premiere;
+
+    public Poster(int id, String name, String genre, String picture, boolean premiere) {
+        this.id = id;
+        this.name = name;
+        this.picture = picture;
+        this.premiere = premiere;
+    }
+
+}
